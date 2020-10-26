70ab8fafcc3b967a3c679c63338b41e2a66d3865
Prograwiz <prograwiz@protonmail.com>
2020-10-26 08:16:08 +0100

    Add ConsoleApp #1

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(StartOfTheSong());
+
+            string StartOfTheSong()
+            {
+                string lyrics = "Start of the song!" + Environment.NewLine + Environment.NewLine;
+
+                // Lyrics of the song
+                for (int i = 99; i > 0; i--)
+                {
+                    lyrics += Environment.NewLine + i + $" bottle{(i == 1 ? "" : "s")} of beer on the wall,";
+                    lyrics += Environment.NewLine + i + $" bottle{(i == 1 ? "" : "s")} of beer!";
+                    lyrics += Environment.NewLine + "Take one down,";
+                    lyrics += Environment.NewLine + "Pass it around,";
+                    lyrics += Environment.NewLine +
+                              $"{(i - 1 == 0 ? "No more bottles" : i - 1 + $" bottle{(i - 1 == 1 ? "" : "s")}")} of beer on the wall!" +
+                              Environment.NewLine;
+                }
+
+                lyrics += Environment.NewLine + Environment.NewLine + "End of the song!";
+
+                return lyrics;
+            }
+        }
+    }
+}
